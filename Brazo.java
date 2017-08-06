ac126cd49e43e5a465eaa986fdcac56f138242a0
OSCAR FERNANDO CASTRILLON QUINTERO <ocastril2387@correo.policia.gov.co>
2017-08-06 07:20:09 -0500

    Intercambio inicial de proyecto

---
+++ Brazo.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Teta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Brazo extends Partes_del_Cuerpo 
+
+{
+   private Manos p;
+   private Anillo t; 
+    public Brazo ()
+   {
+     p = new Manos ();
+     t = new Anillo ();
+    }
+}
---
+++ Manos.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Pezon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Manos extends Partes_del_Cuerpo
+{
+   
+}
